b0f2b5a58a1d17a5d655c38c6c0f5a385c6c371e
et120 <e.trotter120@gmail.com>
2024-03-28 17:49:37 -0700

    localStorage is not defined during the build process

--- src/app/utils/LocalStorage.ts
+++ src/app/utils/LocalStorage.ts
@@ -1,82 +1,89 @@
-'use client'
-
 const saveFavoriteToLocalStorage = (location: string) => {
+    if (typeof window !== 'undefined') {
+        let favorites = getFavoriteLocalStorage();
 
-    let favorites = getFavoriteLocalStorage();
+        if(!favorites.includes(location)){
+            favorites.push(location);
+        }
 
-    if(!favorites.includes(location)){
-        favorites.push(location);
+        localStorage.setItem("Favorites", JSON.stringify(favorites));
     }
-
-    localStorage.setItem("Favorites", JSON.stringify(favorites));
 };
 
 const getFavoriteLocalStorage = () => {
+    if (typeof window !== 'undefined') {
+        let localStorageData = localStorage.getItem("Favorites");
 
-    let localStorageData = localStorage.getItem("Favorites");
+        if(localStorageData == null){
+            return [];
+        }
 
-    if(localStorageData == null){
-        return [];
+        return JSON.parse(localStorageData);
     }
-
-    return JSON.parse(localStorageData);
+    return [];
 };
 
 const removeFavoriteFromLocalStorage = (location: string) => {
+    if (typeof window !== 'undefined') {
+        let favorites = getFavoriteLocalStorage();
+        let namedIndex = favorites.indexOf(location);
 
-    let favorites = getFavoriteLocalStorage();
-    let namedIndex = favorites.indexOf(location);
-
-    favorites.splice(namedIndex, 1);
-    localStorage.setItem("Favorites", JSON.stringify(favorites));
+        favorites.splice(namedIndex, 1);
+        localStorage.setItem("Favorites", JSON.stringify(favorites));
+    }
 };
 
 const saveRecentToLocalStorage = (location: string) => {
-    let recents = getRecentLocalStorage();
+    if (typeof window !== 'undefined') {
+        let recents = getRecentLocalStorage();
 
-    // Check if location already exists in recents array
-    const index = recents.indexOf(location);
+        // Check if location already exists in recents array
+        const index = recents.indexOf(location);
 
-    // If location exists and it's not already at index 0
-    if (index !== -1 && index !== 0) {
-        // Remove existing entry and add it to the beginning of the array
-        recents.splice(index, 1);
-        recents.unshift(location);
-    } else if (index === 0) {
-        // If location is already at index 0, no need to change anything
-        return;
-    } else {
-        // If location is not in the array or is already at index 0
+        // If location exists and it's not already at index 0
+        if (index !== -1 && index !== 0) {
+            // Remove existing entry and add it to the beginning of the array
+            recents.splice(index, 1);
+            recents.unshift(location);
+        } else if (index === 0) {
+            // If location is already at index 0, no need to change anything
+            return;
+        } else {
+            // If location is not in the array or is already at index 0
 
-        // Remove the last element if the array length exceeds 3
-        if (recents.length >= 3) {
-            recents.pop();
+            // Remove the last element if the array length exceeds 3
+            if (recents.length >= 3) {
+                recents.pop();
+            }
+            // Add location to the beginning of the array
+            recents.unshift(location);
         }
-        // Add location to the beginning of the array
-        recents.unshift(location);
-    }
 
-    localStorage.setItem("Recents", JSON.stringify(recents));
+        localStorage.setItem("Recents", JSON.stringify(recents));
+    }
 };
 
 const getRecentLocalStorage = () => {
+    if (typeof window !== 'undefined') {
+        let localStorageData = localStorage.getItem("Recents");
 
-    let localStorageData = localStorage.getItem("Recents");
+        if(localStorageData == null){
+            return [];
+        }
 
-    if(localStorageData == null){
-        return [];
+        return JSON.parse(localStorageData);
     }
-
-    return JSON.parse(localStorageData);
+    return [];
 };
 
 const removeRecentFromLocalStorage = (location: string) => {
+    if (typeof window !== 'undefined') {
+        let recents = getRecentLocalStorage();
+        let namedIndex = recents.indexOf(location);
 
-    let recents = getRecentLocalStorage();
-    let namedIndex = recents.indexOf(location);
-
-    recents.splice(namedIndex, 1);
-    localStorage.setItem("Recents", JSON.stringify(recents));
+        recents.splice(namedIndex, 1);
+        localStorage.setItem("Recents", JSON.stringify(recents));
+    }
 };
 
 export { saveFavoriteToLocalStorage, getFavoriteLocalStorage, removeFavoriteFromLocalStorage, saveRecentToLocalStorage, getRecentLocalStorage, removeRecentFromLocalStorage };
